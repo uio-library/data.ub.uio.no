3ba0e3070f0c61adb2c6e163a5ba6f3eec4a2bfd
Dan Michael O. Heggø <danmichaelo@gmail.com>
2016-03-08 22:14:43 +0100

    Add https support
    - Show notice for yasqe and lodlive, since we cannot use https with
      these for upstream reasons.

--- apache/install.sh
+++ apache/install.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-\cp 000-default.conf /etc/httpd/conf.d/000-default.conf
+\cp data_ub_uio_no.conf /etc/httpd/conf.d/data_ub_uio_no.conf
+\cp ssl.conf /etc/httpd/conf.d/ssl.conf
+\cp status.conf /etc/httpd/conf.d/status.conf
 \cp 01-deflate.conf /etc/httpd/conf.modules.d/01-deflate.conf
+
+mkdir -p /etc/httpd/includes
+\cp data_ub_uio_no /etc/httpd/includes/data_ub_uio_no
+
 
 sed -i 's|DocumentRoot "/var/www/html"|DocumentRoot "/opt/data.ub/www/default"|' /etc/httpd/conf/httpd.conf
 sed -i 's|<Directory "/var/www">|<Directory "/opt/data.ub/www">|' /etc/httpd/conf/httpd.conf
