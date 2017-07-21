dedf5f592dfdb09a0058ab865ce0c330f7ba97ac
Dan Michael O. Heggø <danmichaelo@gmail.com>
2017-07-21 19:27:52 +0200

    Add landing page for ubprod01-uxl.uio.no

---
+++ www/uxl/index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>uxl</title>
+
+  <style>
+   body {
+     background: #efefef;
+     color: #333;
+     white-space: nowrap;
+     font-family: sans-serif;
+     font-size: 13px;
+     padding: 1em;
+   }
+  </style>
+
+
+</head>
+
+<body>
+
+Intet her enda.
+
+ 
+</body>
+</html>
