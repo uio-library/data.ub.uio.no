4ee7add2f2e3f82ba9a6de942a76379faf8f20c2
Dan Michael O. Heggø <danmichaelo@gmail.com>
2017-07-21 19:18:28 +0200

    Update check_diskspace.sh

--- check_diskspace.sh
+++ check_diskspace.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-df | grep / | while read -r line; do
+df -h | grep / | while read -r line; do
   partition="$(echo $line | awk '{ print $1 }')"
+  avail="$(echo $line | awk '{ print $4 }')"
   usep="$(echo $line | awk '{ print $5 }' | cut -d'%' -f1)"
-  if [ $usep -ge 90 ]; then
-    echo "Running out of space \"$partition ($usep%)\" on $(hostname) as on $(date)" | mail -s "Alert: Almost out of disk space $usep%" dmheggo
+  if [ $usep -ge 92 ]; then
+    printf "Host: %s\nPartition: %s\nSpace left: %s" "$(hostname)" "$partition" "$avail" | mail -s "Alert: Partition on $(hostname) is $usep% full" dmheggo
   fi
 done
-
