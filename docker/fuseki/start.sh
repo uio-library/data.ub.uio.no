50b8b73162222ab83b4cdd4237f295767a7a6f8c
Dan Michael O. Heggø <danmichaelo@gmail.com>
2019-07-28 17:16:06 +0200

    Fix that Fuseki admin password is set

--- docker/fuseki/start.sh
+++ docker/fuseki/start.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
-cp /jena-fuseki/shiro.ini /fuseki/shiro.ini
-exec /jena-fuseki/fuseki-server --update --config config.ttl
+cp $FUSEKI_HOME/shiro.ini $FUSEKI_BASE/shiro.ini
+# $ADMIN_PASSWORD can always override
+if [ -n "$ADMIN_PASSWORD" ] ; then
+  echo "Setting admin password"
+  sed -i "s/^admin=.*/admin=$ADMIN_PASSWORD/" "$FUSEKI_BASE/shiro.ini"
+fi
+exec $FUSEKI_HOME/fuseki-server --update --config config.ttl
 
